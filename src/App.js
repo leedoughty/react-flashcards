2fbd7c46c1728dc4874ed348849c0b490709daf2
Lee Doughty <leedoughtydesign@gmail.com>
2019-02-11 09:21:01 +0000

    Add state for cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import Card from './Card/Card';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: [
+        {id: 1, jpn: "Japanese", eng: "English", pronunciation: "Pronunciation" },
+        {id: 2, jpn: "Japanese-2", eng: "English-2", pronunciation: "Pronunciation-2" },
+      ],
+      currentCard: {}
+    }
+  }
+
   render() {
     return (
       <div className="App">
